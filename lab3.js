80a862d8403eb59b31f681cedb2c5f57999bb4d4
alinamollishteanu-coder <alina.mollishteanu@gmail.com>
2026-05-05 23:19:35 +0300

    Add files via upload

---
+++ lab3.js
@@ -0,0 +1,31 @@
+const transactions = [
+    
+     {
+      transaction_id: "1",
+      transaction_date: "2019-01-01",
+      transaction_amount: 100.0,
+      transaction_type: "debit",
+      transaction_description: "Payment for groceries",
+      merchant_name: "SuperMart",
+      card_type: "Visa",
+    },
+    {
+      transaction_id: "2",
+      transaction_date: "2019-01-02",
+      transaction_amount: 50.0,
+      transaction_type: "credit",
+      transaction_description: "Refund for returned item",
+      merchant_name: "OnlineShop",
+      card_type: "MasterCard",
+    },
+    {
+      transaction_id: "3",
+      transaction_date: "2019-01-03",
+      transaction_amount: 75.0,
+      transaction_type: "debit",
+      transaction_description: "Dinner with friends",
+      merchant_name: "RestaurantABC",
+      card_type: "Amex",
+    }
+    
+]
